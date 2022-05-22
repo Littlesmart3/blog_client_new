9c35caebcef08ab21af7d5d7088d3b8d61cd8ef9
范力 <798347235@qq.com>
2022-05-22 12:19:00 +0800

    feat: 项目更新为next.js，支持ssr服务端渲染

--- script/build.sh
+++
@@ -1,3 +0,0 @@
-cross-env NODE_ENV=prod webpack --config ./script/config/webpack.prod.js  
-
-
--- src/render/store/index.ts
+++
@@ -1,7 +0,0 @@
-// src/store/index.ts
-import React from "react";
-import loginStore from "./modules/login";
-
-export const storesContext = React.createContext({
-  loginStore,
-});
