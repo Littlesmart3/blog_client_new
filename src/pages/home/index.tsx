e41327f7a78db525c97c17907b5802c521c96c96
范力 <798347235@qq.com>
2022-06-15 17:18:34 +0800

    fix: 路由重定向修改

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
+import React, { useEffect } from 'react';
+import Router from 'next/router';
 import Image from 'next/image';
 import backgroundImg from '@/assets/img/background.jpeg';
 import style from './style.module.scss';
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const { pathname } = Router;
+    if (pathname == '/home') Router.push('/');
+  });
   return (
     <div className={style.home}>
       <Image src={backgroundImg || ''} alt='background-img' objectFit='cover' layout='fill' quality='100' />
